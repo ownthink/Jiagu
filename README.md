f3c8e484ddbe48939b0695a46372e3da91581c46
Yener <help@ownthink.com>
2019-01-06 20:37:38 +0800

    dynamic init

--- README.md
+++ README.md
@@ -27,7 +27,24 @@ cd Jiagu
 python3 setup.py install
 ```
 
+### 使用方式
+```python3
+import jiagu
 
+#jiagu.init() # 可手动初始化，也可以动态初始化
+
+
+text = '厦门明天会不会下雨'
+
+words = jiagu.seg(text) # 分词
+print(words)
+
+pos = jiagu.pos(words) # 词性标注
+print(pos)
+
+ner = jiagu.ner(text) # 命名实体识别
+print(ner)
+```
 
 
 
--- test.py
+++ test.py
@@ -1,20 +1,15 @@
 import jiagu
 
-#jiagu.init()
+#jiagu.init() # 可手动初始化，也可以动态初始化
 
 
 text = '厦门明天会不会下雨'
 
-words = jiagu.seg(text)
+words = jiagu.seg(text) # 分词
 print(words)
 
-pos = jiagu.pos(words)
+pos = jiagu.pos(words) # 词性标注
 print(pos)
 
-ner = jiagu.ner(text)
+ner = jiagu.ner(text) # 命名实体识别
 print(ner)
-
-
-
-
-
