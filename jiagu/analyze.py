ae740286adab8772db86af96108af838f475ce5a
zengbin93 <zeng_bin8888@163.com>
2019-01-09 16:20:25 +0800

    apply to pep8

--- jiagu/analyze.py
+++ jiagu/analyze.py
@@ -1,176 +1,174 @@
-'''
- * Copyright (C) 2017 OwnThink Technologies Inc. 
+"""
+ * Copyright (C) 2017 OwnThink Technologies Inc.
  *
  * Name        : analyze.py - 解析
  * Author      : Yener <yener@ownthink.com>
  * Version     : 0.01
  * Description : 解析模块
-'''
+"""
 import os
-import sys
-import json
 from collections import defaultdict
 from jiagu import bilstm_crf
 from jiagu import textrank
 
+
 def add_curr_dir(name):
-	return os.path.join(os.path.dirname(__file__), name)
-						
-class Analyze():
-	def __init__(self):
-		self.seg_model = None
-		self.pos_model = None
-		self.ner_model = None
-		self.init_flag = 0
-		
-	def init(self):
-		self.init_cws() #4
-		self.init_pos() #2
-		self.init_ner() #1
-		
-	def init_cws(self):
-		if self.seg_model == None:
-			self.seg_model = bilstm_crf.Predict(add_curr_dir('model/cws.model'))
-			self.init_flag += 4
-		
-	def init_pos(self):
-		if self.pos_model == None:
-			self.pos_model = bilstm_crf.Predict(add_curr_dir('model/pos.model'))
-			self.init_flag += 2
-		
-	def init_ner(self):
-		if self.ner_model == None:
-			self.ner_model = bilstm_crf.Predict(add_curr_dir('model/ner.model'))
-			self.init_flag += 1
-	
-	def __lab2word(self, sentence, labels):
-		words = []
-		N = len(sentence)
-		tmp_word = ""
-		for i in range(N):
-			label = labels[i]
-			w = sentence[i]
-			if label == "B":
-				tmp_word += w
-			elif label == "M":
-				tmp_word += w
-			elif label == "E":
-				tmp_word += w
-				words.append(tmp_word)
-				tmp_word = ""
-			else:
-				tmp_word = ""
-				words.append(w)
-		if tmp_word:
-			words.append(tmp_word)
-		return words
-	
-	def cws_text(self, sentence):
-		labels = self.seg_model.predict([sentence])[0]
-		return self.__lab2word(sentence, labels)
-	
-	def cws_list(self, sentences):
-		text_list = sentences
-		all_labels = self.seg_model.predict(text_list)
-		sent_words = []
-		for ti, text in enumerate(text_list):
-			seg_labels = all_labels[ti]
-			sent_words.append(self.__lab2word(text, seg_labels))
-		return sent_words
-		
-	def cws(self, sentence, input='text'):#传入的是文本
-		if self.init_flag not in [4, 5, 6, 7]:
-			self.init_cws()
-			
-		if input == 'batch':
-			words_list = self.cws_list(sentence)
-			return words_list
-		else:
-			words = self.cws_text(sentence)
-			return words
-		
-	def pos(self, sentence, input='text'):#传入的是词语
-		if self.init_flag not in [2, 3, 6, 7]:
-			self.init_pos()
-			
-		if input == 'batch':
-			all_labels = self.pos_model.predict(sentence)
-			return all_labels
-		else:
-			labels = self.pos_model.predict([sentence])[0]
-			return labels
-			
-	def ner(self, sentence, input='text'):#传入的是文本
-		if self.init_flag not in [1, 3, 5, 7]:
-			self.init_ner()
-			
-		if input == 'batch':
-			all_labels = self.ner_model.predict(sentence)
-			return all_labels
-		else:
-			labels = self.ner_model.predict([sentence])[0]
-			return labels
-		
-	def cws_pos(self, sentence):
-		text_list = sentence
-		all_labels = self.pos_model.predict(text_list)
-		return all_labels
-	
-	def cws_pos_ner(self, sentences):
-		seg_list = self.seg(sentences)
-		pos_list = self.pos(seg_list)
-		ner_list = self.ner(sentences)
-		
-		results = []
-		for i in range(len(sentences)):
-			words = seg_list[i]
-			pos = pos_list[i]
-			ner = ner_list[i]
-			results.append([words, pos, ner])
+    return os.path.join(os.path.dirname(__file__), name)
 
-		# listword = []
-		# for word, net in zip(sentences, ner):
-			# if net[0] == 'B':
-				# listword.append(word)
-			# elif net[0] == 'I' or net[0] == 'E':
-				# listword[len(listword)-1]+=word
-			# else:
-				# listword.append(word)
 
-			# results.append([words, pos, listword])
+class Analyze(object):
+    def __init__(self):
+        self.seg_model = None
+        self.pos_model = None
+        self.ner_model = None
+        self.init_flag = 0
 
-		return results
-	
-	def keywords(self, words, topkey=5):
-		'''
+    def init(self):
+        self.init_cws()  # 4
+        self.init_pos()  # 2
+        self.init_ner()  # 1
+
+    def init_cws(self):
+        if self.seg_model is None:
+            self.seg_model = bilstm_crf.Predict(add_curr_dir('model/cws.model'))
+            self.init_flag += 4
+
+    def init_pos(self):
+        if self.pos_model is None:
+            self.pos_model = bilstm_crf.Predict(add_curr_dir('model/pos.model'))
+            self.init_flag += 2
+
+    def init_ner(self):
+        if self.ner_model is None:
+            self.ner_model = bilstm_crf.Predict(add_curr_dir('model/ner.model'))
+            self.init_flag += 1
+
+    @staticmethod
+    def __lab2word(sentence, labels):
+        words = []
+        N = len(sentence)
+        tmp_word = ""
+        for i in range(N):
+            label = labels[i]
+            w = sentence[i]
+            if label == "B":
+                tmp_word += w
+            elif label == "M":
+                tmp_word += w
+            elif label == "E":
+                tmp_word += w
+                words.append(tmp_word)
+                tmp_word = ""
+            else:
+                tmp_word = ""
+                words.append(w)
+        if tmp_word:
+            words.append(tmp_word)
+        return words
+
+    def cws_text(self, sentence):
+        labels = self.seg_model.predict([sentence])[0]
+        return self.__lab2word(sentence, labels)
+
+    def cws_list(self, sentences):
+        text_list = sentences
+        all_labels = self.seg_model.predict(text_list)
+        sent_words = []
+        for ti, text in enumerate(text_list):
+            seg_labels = all_labels[ti]
+            sent_words.append(self.__lab2word(text, seg_labels))
+        return sent_words
+
+    def cws(self, sentence, input='text'):  # 传入的是文本
+        if self.init_flag not in [4, 5, 6, 7]:
+            self.init_cws()
+
+        if input == 'batch':
+            words_list = self.cws_list(sentence)
+            return words_list
+        else:
+            words = self.cws_text(sentence)
+            return words
+
+    def pos(self, sentence, input='text'):  # 传入的是词语
+        if self.init_flag not in [2, 3, 6, 7]:
+            self.init_pos()
+
+        if input == 'batch':
+            all_labels = self.pos_model.predict(sentence)
+            return all_labels
+        else:
+            labels = self.pos_model.predict([sentence])[0]
+            return labels
+
+    def ner(self, sentence, input='text'):  # 传入的是文本
+        if self.init_flag not in [1, 3, 5, 7]:
+            self.init_ner()
+
+        if input == 'batch':
+            all_labels = self.ner_model.predict(sentence)
+            return all_labels
+        else:
+            labels = self.ner_model.predict([sentence])[0]
+            return labels
+
+    def cws_pos(self, sentence):
+        text_list = sentence
+        all_labels = self.pos_model.predict(text_list)
+        return all_labels
+
+    def cws_pos_ner(self, sentences):
+        seg_list = self.seg(sentences)
+        pos_list = self.pos(seg_list)
+        ner_list = self.ner(sentences)
+
+        results = []
+        for i in range(len(sentences)):
+            words = seg_list[i]
+            pos = pos_list[i]
+            ner = ner_list[i]
+            results.append([words, pos, ner])
+
+        # listword = []
+        # for word, net in zip(sentences, ner):
+        # if net[0] == 'B':
+        # listword.append(word)
+        # elif net[0] == 'I' or net[0] == 'E':
+        # listword[len(listword)-1]+=word
+        # else:
+        # listword.append(word)
+
+        # results.append([words, pos, listword])
+
+        return results
+
+    @staticmethod
+    def keywords(words, topkey=5):
+        """
 		关键字抽取
-		'''
-		g = textrank.TextRank()
+		"""
+        g = textrank.TextRank()
 
-		cm = defaultdict(int)
-		span = 5
-		for i, wp in enumerate(words):
-			for j in range(i + 1, i + span):
-				if j >= len(words):
-					break
-				cm[(wp, words[j])] += 1
-				
-		for terms, w in cm.items():
-			g.addEdge(terms[0], terms[1], w)
-		
-		nodes_rank = g.rank()
+        cm = defaultdict(int)
+        span = 5
+        for i, wp in enumerate(words):
+            for j in range(i + 1, i + span):
+                if j >= len(words):
+                    break
+                cm[(wp, words[j])] += 1
 
-		withWeight = False
-		if withWeight:
-			tags = sorted(nodes_rank.items(), key=operator.itemgetter(1), reverse=True)
-		else:
-			tags = sorted(nodes_rank, key=nodes_rank.__getitem__, reverse=True)
+        for terms, w in cm.items():
+            g.addEdge(terms[0], terms[1], w)
 
-		return tags[:topkey]
-	
-	def abstract(self, text):
-		pass
-		
-		
-		
-	
+        nodes_rank = g.rank()
+
+        withWeight = False
+        if withWeight:
+            tags = sorted(nodes_rank.items(), key=operator.itemgetter(1), reverse=True)
+        else:
+            tags = sorted(nodes_rank, key=nodes_rank.__getitem__, reverse=True)
+
+        return tags[:topkey]
+
+    def abstract(self, text):
+        pass
--- jiagu/bilstm_crf.py
+++ jiagu/bilstm_crf.py
@@ -1,78 +1,79 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import sys
 import pickle
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.crf import viterbi_decode
 
+
 class Predict(object):
-	def __init__(self, model_file, char_to_id={}, id_to_tag={}, use_pack=True):
-		if use_pack:
-			with open(model_file, 'rb') as f:
-				model, char_to_id, id_to_tag = pickle.load(f)
-		else:
-			model = model_file
-		
-		self.char_to_id = char_to_id
-		self.id_to_tag = {int(k):v for k,v in id_to_tag.items()}
-		self.graph = self.load_graph(model, use_pack)
+    def __init__(self, model_file, char_to_id={}, id_to_tag={}, use_pack=True):
+        if use_pack:
+            with open(model_file, 'rb') as f:
+                model, char_to_id, id_to_tag = pickle.load(f)
+        else:
+            model = model_file
 
-		self.input_x = self.graph.get_tensor_by_name("prefix/char_inputs:0")
-		self.lengths = self.graph.get_tensor_by_name("prefix/lengths:0")
-		self.dropout = self.graph.get_tensor_by_name("prefix/dropout:0")
-		self.logits = self.graph.get_tensor_by_name("prefix/project/logits:0")
-		self.trans = self.graph.get_tensor_by_name("prefix/crf_loss/transitions:0")
+        self.char_to_id = char_to_id
+        self.id_to_tag = {int(k): v for k, v in id_to_tag.items()}
+        self.graph = self.load_graph(model, use_pack)
 
-		self.sess = tf.Session(graph=self.graph)
-		self.sess.as_default()
-		self.num_class = len(self.id_to_tag)
+        self.input_x = self.graph.get_tensor_by_name("prefix/char_inputs:0")
+        self.lengths = self.graph.get_tensor_by_name("prefix/lengths:0")
+        self.dropout = self.graph.get_tensor_by_name("prefix/dropout:0")
+        self.logits = self.graph.get_tensor_by_name("prefix/project/logits:0")
+        self.trans = self.graph.get_tensor_by_name("prefix/crf_loss/transitions:0")
 
-	def load_graph(self, model_file, use_pack):
-		if use_pack:
-			graph_def = tf.GraphDef()
-			graph_def.ParseFromString(model_file)
-		else:
-			with tf.gfile.GFile(model_file, "rb") as f:
-				graph_def = tf.GraphDef()
-				graph_def.ParseFromString(f.read())
-		with tf.Graph().as_default() as graph:
-			tf.import_graph_def(graph_def, name="prefix")
-		return graph
+        self.sess = tf.Session(graph=self.graph)
+        self.sess.as_default()
+        self.num_class = len(self.id_to_tag)
 
-	def decode(self, logits, trans, sequence_lengths, tag_num):
-		viterbi_sequences = []
-		small = -1000.0
-		start = np.asarray([[small] * tag_num + [0]])
-		for logit, length in zip(logits, sequence_lengths):
-			score = logit[:length]
-			pad = small * np.ones([length, 1])
-			score = np.concatenate([score, pad], axis=1)
-			score = np.concatenate([start, score], axis=0)
-			viterbi_seq, viterbi_score = viterbi_decode(score, trans)
-			viterbi_sequences.append(viterbi_seq[1:])
-		return viterbi_sequences
+    def load_graph(self, model_file, use_pack):
+        if use_pack:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(model_file)
+        else:
+            with tf.gfile.GFile(model_file, "rb") as f:
+                graph_def = tf.GraphDef()
+                graph_def.ParseFromString(f.read())
+        with tf.Graph().as_default() as graph:
+            tf.import_graph_def(graph_def, name="prefix")
+        return graph
 
-	def predict(self, sents):
-		inputs = []
-		lengths = [len(text) for text in sents]
-		max_len = max(lengths)
+    def decode(self, logits, trans, sequence_lengths, tag_num):
+        viterbi_sequences = []
+        small = -1000.0
+        start = np.asarray([[small] * tag_num + [0]])
+        for logit, length in zip(logits, sequence_lengths):
+            score = logit[:length]
+            pad = small * np.ones([length, 1])
+            score = np.concatenate([score, pad], axis=1)
+            score = np.concatenate([start, score], axis=0)
+            viterbi_seq, viterbi_score = viterbi_decode(score, trans)
+            viterbi_sequences.append(viterbi_seq[1:])
+        return viterbi_sequences
 
-		for sent in sents:
-			sent_ids = [self.char_to_id.get(w) if w in self.char_to_id else self.char_to_id.get("<OOV>") for w in sent]
-			padding = [0] * (max_len - len(sent_ids))
-			sent_ids += padding
-			inputs.append(sent_ids)
-		inputs = np.array(inputs, dtype=np.int32)
+    def predict(self, sents):
+        inputs = []
+        lengths = [len(text) for text in sents]
+        max_len = max(lengths)
 
-		feed_dict = {
-			self.input_x: inputs,
-			self.lengths: lengths,
-			self.dropout: 1.0
-		}
+        for sent in sents:
+            sent_ids = [self.char_to_id.get(w) if w in self.char_to_id else self.char_to_id.get("<OOV>") for w in sent]
+            padding = [0] * (max_len - len(sent_ids))
+            sent_ids += padding
+            inputs.append(sent_ids)
+        inputs = np.array(inputs, dtype=np.int32)
 
-		logits, trans = self.sess.run([self.logits, self.trans], feed_dict=feed_dict)
-		path = self.decode(logits, trans, lengths, self.num_class)
-		labels = [[self.id_to_tag.get(l) for l in p] for p in path]
-		return labels
-		
+        feed_dict = {
+            self.input_x: inputs,
+            self.lengths: lengths,
+            self.dropout: 1.0
+        }
+
+        logits, trans = self.sess.run([self.logits, self.trans], feed_dict=feed_dict)
+        path = self.decode(logits, trans, lengths, self.num_class)
+        labels = [[self.id_to_tag.get(l) for l in p] for p in path]
+        return labels
