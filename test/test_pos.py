f3907c18d6e4ae2f7e30df29d61d0416febe57ce
zhangmiao <zmiao@mail.ustc.edu.cn>
2019-02-27 17:16:04 +0800

    update

--- test/test_pos.py
+++ test/test_pos.py
@@ -2,9 +2,9 @@ import jiagu
 
 text = '厦门明天的天气怎么样'
 
-words = jiagu.seg(text) # 分词
+words = jiagu.seg(text)  # 分词
 print(words)
 
-pos = jiagu.pos(words) # 词性标注
+pos = jiagu.pos(words)  # 词性标注
 print(pos)
 
