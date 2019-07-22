669f246824c0d39afa03da2b12d15a7d04a6c7f6
Yener <help@ownthink.com>
2019-07-22 16:58:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='jiagu',
       author_email='help@ownthink.com',
       url='https://github.com/ownthink/Jiagu',
       license='MIT',
-      install_requires=['tensorflow>=1.4.0', 'numpy>=1.12.1'],
+      install_requires=['tensorflow>=1.6.0', 'numpy>=1.12.1'],
       packages=['jiagu'],
       package_dir={'jiagu': 'jiagu'},
       package_data={'jiagu': ['*.*', 'model/*', 'data/*']}
