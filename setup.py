85a4de875c15ed112ebf818de096fe28791656ee
Yener <help@ownthink.com>
2020-01-01 15:17:23 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='jiagu',
-      version='0.2.2',
+      version='0.2.3',
       description='Jiagu Natural Language Processing',
       author='Yener(Zheng Wenyu)',
       author_email='help@ownthink.com',
